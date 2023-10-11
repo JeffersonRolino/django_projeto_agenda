fb3fc5e246f6c541e2e10a8737480e4eb222a8e8
jeffersonrolino <jefferson.rolino@gmail.com>
2023-10-11 02:17:27 -0300

    Usando um padrão comum para URLs de CRUD

--- contact/urls.py
+++ contact/urls.py
@@ -5,6 +5,11 @@ app_name = 'contact'
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('<int:contact_id>/', views.contact, name='contact'),
     path('search/', views.search, name='search'),
+
+    # contact (CRUD)
+    path('contact/<int:contact_id>/detail/', views.contact, name='contact'),
+    # path('contact/create/', views.contact, name='contact'),
+    # path('contact/<int:contact_id>/update/', views.contact, name='contact'),
+    # path('contact/<int:contact_id>/delete/', views.contact, name='contact'),
 ]
